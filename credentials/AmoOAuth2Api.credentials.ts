d7703b6823f7eed4eabca48f920b442ba7731dad
Kirill Chertkov <chertkov.k@gmail.com>
2023-09-23 01:20:21 +0300

    add amo credentials

---
+++ credentials/AmoOAuth2Api.credentials.ts
@@ -0,0 +1,83 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class AmoOAuth2Api implements ICredentialType {
+	name = 'amoOAuth2Api';
+	extends = ['oAuth2Api'];
+	displayName = 'AmoCRM OAuth2 API';
+	documentationUrl = 'https://www.amocrm.ru/developers/content/oauth/easy-auth'; // TODO: написать инструкцию
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Grant Type',
+			name: 'grantType',
+			type: 'hidden',
+			default: 'authorizationCode',
+		},
+		{
+			displayName: 'Authorization URL',
+			name: 'authUrl',
+			type: 'hidden',
+			default: 'https://www.amocrm.ru/oauth',
+		},
+		{
+			displayName: 'Access Token URL',
+			name: 'accessTokenUrl',
+			type: 'string',
+			default: '', // TODO: можно ли сделать, чтобы достаточно было ввести домен
+		},
+		{
+			displayName: 'Client ID',
+			name: 'clientId',
+			type: 'string',
+			default: '',
+			required: true,
+		},
+		{
+			displayName: 'Client Secret',
+			name: 'clientSecret',
+			type: 'string',
+			typeOptions: {
+							password: true,
+			},
+			default: '',
+			required: true,
+		},
+		{
+			displayName: 'Auth URI Query Parameters',
+			name: 'authQueryParameters',
+			type: 'string',
+			default: 'mode=post_message',
+		},
+		{
+			displayName: 'Authentication',
+			name: 'authentication',
+			type: 'hidden',
+			default: 'body',
+		},
+	];
+
+	// This allows the credential to be used by other parts of n8n
+	// stating how this credential is injected as part of the request
+	// An example is the Http Request node that can make generic calls
+	// reusing this credential
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '={{"Bearer " + $credentials.token}}',
+			},
+		},
+	};
+
+	// The block below tells how this credential can be tested
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{$credentials?.domain}}',
+			url: '/bearer',
+		},
+	};
+}
